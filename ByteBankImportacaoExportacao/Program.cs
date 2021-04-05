45deb3c0fd3a1a41a85c9697456ee5d002adc4fc
Andre Luis Haeberlin <andrehaeberlin@outlook.com>
2021-04-05 20:56:15 -0300

    Exercises OOP C#
    I did some exercises during the course

---
+++ ByteBankImportacaoExportacao/Program.cs
@@ -0,0 +1,36 @@
+﻿using ByteBankImportacaoExportacao.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBankImportacaoExportacao
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            string enderecoDoArquivo = "contas.txt";
+
+            using (var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
+            using (var leitor = new StreamReader(fluxoDoArquivo)) 
+            { 
+                //ler até o fim
+                //var linha = leitor.ReadToEnd();
+
+                while (!leitor.EndOfStream)
+                {
+                    var linha = leitor.ReadLine();
+                    Console.WriteLine(linha);
+                }
+                
+            }
+
+            Console.ReadLine();
+
+        }
+
+    }
+}
---
+++ ByteBankImportacaoExportacao/TratandoStreamDiretamente.cs
@@ -0,0 +1,55 @@
+﻿using ByteBankImportacaoExportacao.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteBankImportacaoExportacao
+{
+    partial class Program
+    {
+         public void TratandoStreamDiretamente()
+        {
+
+            string enderecoDoArquivo = "contas.txt";
+
+            //metodo using : IDisposable
+            using (var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
+            {
+                var buffer = new byte[1024]; // 1 kb 
+
+                int numeroBytesLidos = -1;
+
+                while (numeroBytesLidos != 0)
+                {
+                    numeroBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
+                    EscreverBuffer(buffer, numeroBytesLidos);
+                }
+            }
+
+            //byte 255 valores
+            //Unicode - cada caracter é um code point
+            //Unicode Transformation Format - UTF
+
+            Console.ReadLine();
+        }
+
+        static void EscreverBuffer(byte[] buffer, int bytesLidos)
+        {
+            var utf8 = Encoding.Default;
+
+            var texto = utf8.GetString(buffer, 0, bytesLidos);
+
+            Console.Write(texto);
+
+            //Show Bytes
+            //foreach (var meuByte in buffer)
+            //{
+            //    Console.Write(meuByte);
+            //    Console.Write(" ");
+            //}
+        }
+    }
+}
